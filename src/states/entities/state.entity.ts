853ad49f45d0b057aee082c85daeab33e77850b9
Jh0nC <jhonatancorrales681@gmail.com>
2025-01-25 01:12:25 -0500

    fix some bugs on states module

--- src/states/entities/state.entity.ts
+++ src/states/entities/state.entity.ts
@@ -1,18 +1,23 @@
+import { Expose } from 'class-transformer';
 import { Role } from 'src/roles/entities/role.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Column, Entity, OneToMany } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class State {
-  @Column({ primary: true, generated: true })
+  @PrimaryGeneratedColumn()
+  @Expose()
   id: number;
 
-  @Column({ length: 30, unique: true, default: false })
+  @Column({ length: 30, unique: true })
+  @Expose()
   name: string;
 
+  /* Do not aappear in table schema */
   @OneToMany(() => Role, (role) => role.id)
   roles: Role[];
 
+  /* Do not aappear in table schema */
   @OneToMany(() => User, (user) => user.id)
   user: User[];
 }
--- src/states/states.service.ts
+++ src/states/states.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateStateDto } from './dto/create-state.dto';
-import { UpdateStateDto } from './dto/update-state.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { State } from './entities/state.entity';
 import { Repository } from 'typeorm';
@@ -12,23 +10,11 @@ export class StatesService {
     private readonly statesReporitory: Repository<State>,
   ) {}
 
-  async create(createStateDto: CreateStateDto) {
-    return await this.statesReporitory.save(createStateDto);
-  }
-
   async findAll() {
     return await this.statesReporitory.find();
   }
 
   async findOne(id: number) {
     return await this.statesReporitory.findOneBy({ id });
-  }
-
-  async update(id: number, updateStateDto: UpdateStateDto) {
-    return `This action updates a #${id} state`;
-  }
-
-  async remove(id: number) {
-    return `This action removes a #${id} state`;
   }
 }
